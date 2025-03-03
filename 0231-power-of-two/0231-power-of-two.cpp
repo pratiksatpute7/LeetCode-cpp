d58ad3bc6fffff4cbb272c4633a940923560c310
Pratik Satpute <58502379+pratiksatpute7@users.noreply.github.com>
2025-03-03 14:05:11 -0500

    Time: 0 ms (100%), Space: 7.8 MB (55.15%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,17 +1,43 @@
 class Solution {
 public:
+    // recursive
+    // bool isPowerOfTwo(int n) {
+    //     if (n == 0) {
+    //         return false;
+    //     }
+    //     if(n == 1) {
+    //         return true;
+    //     }
+
+    //     if(n % 2 == 0) {
+    //         return isPowerOfTwo(n/2);
+    //     }
+
+    //     return false;
+    // }
+    
+
+    //Bit manipulation
+    // bool isPowerOfTwo(int n) {
+    //     if (n == 0) {
+    //         return false;
+    //     }
+    //     if(n == 1) {
+    //         return true;
+    //     }
+
+    //     if((n & 1) == 0) {
+    //         return isPowerOfTwo(n/2);
+    //     }
+
+    //     return false;
+    // }
+
     bool isPowerOfTwo(int n) {
-        if (n == 0) {
-            return false;
-        }
-        if(n == 1) {
-            return true;
-        }
-
-        if(n % 2 == 0) {
-            return isPowerOfTwo(n/2);
-        }
-
-        return false;
+        // in binary if a number is power of 2 only 1 bit is set
+        // there we perform bitwise & with n - 1
+        // in n - 1 all bits are set except the bit set in n
+        // so when we perform add operation result should be equal to 0
+        return (n > 0) && ((n & (n - 1)) == 0);
     }
 };
