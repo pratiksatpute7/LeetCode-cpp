47c3d769639b52519317698eb149c8b00f94662c
Pratik Satpute <58502379+pratiksatpute7@users.noreply.github.com>
2025-03-03 19:03:31 -0500

    Time: 2 ms (42.54%), Space: 9.8 MB (81.55%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -2,14 +2,17 @@ class Solution {
 
 private:
     void subSet(int index, vector<int>& ds, vector<vector<int>>& set, vector<int>& nums, int n) {
+        // base case if index goes beyond size 
         if(index == n) {
             set.push_back(ds);
             return;
         }
 
+        // Take current index
         ds.push_back(nums[index]);
         subSet(index + 1, ds, set, nums, n);
 
+        // sub tree for not take current index
         ds.pop_back();
         subSet(index + 1, ds, set, nums, n);
 
