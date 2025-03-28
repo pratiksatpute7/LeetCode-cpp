c9fedaf2fe2839ba953ecf5eecc7778a8f49b26a
Pratik Satpute <58502379+pratiksatpute7@users.noreply.github.com>
2025-03-27 20:52:07 -0400

    Time: 0 ms (100%), Space: 17.2 MB (37.91%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+        int buy = prices[0];
+
+        for(int i = 1; i < prices.size(); i++) {
+            if(buy > prices[i]) {
+                // there is no profit if the current price is less than buy
+                buy = prices[i];
+            } else {
+                // book profit and set buy to current value
+                // if we are not booking profit the next day that means we never bought it or sold it immedietly
+                profit += prices[i] - buy;
+                buy = prices[i];
+            }
+        }
+
+        return profit;
+    }
+};
