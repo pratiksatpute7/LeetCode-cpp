d8e4b128367a03af65874de2cad681bba04406f4
Pratik Satpute <58502379+pratiksatpute7@users.noreply.github.com>
2025-04-03 22:00:04 -0400

    Time: 3 ms (73.05%), Space: 14.9 MB (36.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,14 +1,53 @@
 class Solution {
 public:
 vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        // vector<int>ans;
+        // int n = nums.size(), l = 0, r = n - 1;
+        // int temp[n];
+        // for(int i=0;i<nums.size();i++){
+        //    temp[i]=nums[i];
+        // }
+        // sort(nums.begin(), nums.end());
+
+        // while(l < r)
+        // {
+        //     int sum = nums[l] + nums[r];
+        //     if(sum == target) {
+        //         break;
+        //     }
+
+        //     if(sum > target) {
+        //         r--;
+        //     } else {
+        //         l++;
+        //     }
+        // }
+
+        // for(int i = 0; i < n; i++){
+        //     if(nums[l] == temp[i]){
+        //         ans.push_back(i);
+        //     }
+        //     else if(nums[r]==temp[i]){
+        //         ans.push_back(i);
+        //     }
+        // }
+
+
+        // return ans;
+
+
+        // using hash set
+        unordered_map<int, int> mp;
+        for(int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if(mp.count(complement)) {
+                return {mp[complement], i};
             }
+
+            mp[nums[i]] = i;
         }
-        return {}; // No solution found
+
+        return {};
     }
 };
