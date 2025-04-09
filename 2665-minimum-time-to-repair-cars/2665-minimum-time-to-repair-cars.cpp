6768871517488d3412071b7816ee99cc4d0e3d8f
Pratik Satpute <58502379+pratiksatpute7@users.noreply.github.com>
2025-04-09 19:53:41 -0400

    Time: 38 ms (57.67%), Space: 59.4 MB (21.39%) - LeetHub

---
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long repairCars(vector<int>& ranks, int cars) {
+       long long start = 1; 
+       long long end = 1LL * (*min_element(ranks.begin(), ranks.end())) * 1LL * cars * cars;
+
+        while(start < end) {
+            long long mid = start + (end - start) / 2;
+            long long totalCars = 0;
+            for(int rank : ranks) {
+                totalCars += (long long)(sqrt((double)mid / rank));
+            }
+
+            if(totalCars >= cars) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    }
+};
