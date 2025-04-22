14566f4bcdd55296b76a11fc91a245ee168b72ab
Pratik Satpute <58502379+pratiksatpute7@users.noreply.github.com>
2025-04-22 19:46:20 -0400

    Time: 0 ms (100%), Space: 8.2 MB (29.82%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        // leftMin min number of left parenthesis
+        // leftMax max number of left parenthesis
+        int leftMin = 0, leftMax = 0;
+
+        for (char c : s) {
+            if (c == '(') {
+                leftMin++;
+                leftMax++;
+            } else if (c == ')') {
+                leftMin--;
+                leftMax--;
+            } else {
+                leftMin--;
+                leftMax++;
+            }
+            if (leftMax < 0) {
+                return false;
+            }
+            if (leftMin < 0) {
+                leftMin = 0;
+            }
+        }
+        return leftMin == 0;
+    }
+};
