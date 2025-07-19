169a636910a24533bfb1619b0aac9cb5a693ef38
Pratik Satpute <58502379+pratiksatpute7@users.noreply.github.com>
2025-07-19 18:24:04 -0400

    Time: 0 ms (100%), Space: 7.7 MB (64.14%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,10 +1,41 @@
 class Solution {
 public:
+    // int fib(int n) {
+    //     vector<int> dp(n + 1, -1);
+    //     dp[0] = 0;
+    //     if (n > 0) dp[1] = 1;    
+    //     return temp(n, dp);
+    // }
+
+    // Tabluation 
+    // int fib(int n) {
+    //     vector<int> dp(n + 1, -1);
+    //     dp[0] = 0;
+    //     if (n > 0) dp[1] = 1;    
+
+    //     for(int i = 2; i <= n; i ++) {
+    //         dp[i] = dp[i - 1] + dp[i - 2];
+    //     }
+
+    //     return dp[n];
+    // }
+
+
+    // Tabluation space optimization
     int fib(int n) {
-        vector<int> dp(n + 1, -1);
-        dp[0] = 0;
-        if (n > 0) dp[1] = 1;    
-        return temp(n, dp);
+        // vector<int> dp(n + 1, -1);
+        // dp[0] = 0;
+        int prev1 = 1;
+        int prev2 = 0;
+        // if (n > 0) dp[1] = 1;    
+
+        for(int i = 0; i < n; i ++) {
+            int cur = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return prev2;
     }
 
     int temp(int n, vector<int>& dp) {
