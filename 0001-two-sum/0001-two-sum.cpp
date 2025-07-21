ff5e111920d093335569b709cf93de56513bcff3
Pratik Satpute <58502379+pratiksatpute7@users.noreply.github.com>
2025-07-21 19:09:53 -0400

    Time: 4 ms (57.22%), Space: 14.8 MB (36.66%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,50 +1,13 @@
 class Solution {
 public:
 vector<int> twoSum(vector<int>& nums, int target) {
-        // vector<int>ans;
-        // int n = nums.size(), l = 0, r = n - 1;
-        // int temp[n];
-        // for(int i=0;i<nums.size();i++){
-        //    temp[i]=nums[i];
-        // }
-        // sort(nums.begin(), nums.end());
+         unordered_map<int, int> mp;
 
-        // while(l < r)
-        // {
-        //     int sum = nums[l] + nums[r];
-        //     if(sum == target) {
-        //         break;
-        //     }
-
-        //     if(sum > target) {
-        //         r--;
-        //     } else {
-        //         l++;
-        //     }
-        // }
-
-        // for(int i = 0; i < n; i++){
-        //     if(nums[l] == temp[i]){
-        //         ans.push_back(i);
-        //     }
-        //     else if(nums[r]==temp[i]){
-        //         ans.push_back(i);
-        //     }
-        // }
-
-
-        // return ans;
-
-
-        // using hash set
-        unordered_map<int, int> mp;
         for(int i = 0; i < nums.size(); i++) {
             int complement = target - nums[i];
-
-            if(mp.count(complement)) {
+            if(mp.find(complement) != mp.end()) {
                 return {mp[complement], i};
             }
-
             mp[nums[i]] = i;
         }
 
