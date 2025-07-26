ca63d9bbb58aad32bdd759192f616920f5cf6da8
Pratik Satpute <58502379+pratiksatpute7@users.noreply.github.com>
2025-07-26 12:27:28 -0400

    Time: 0 ms (100%), Space: 13.1 MB (66.47%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -22,29 +22,57 @@ public:
     //     return dp[i][j] = min(down, diag);
     // }
 
+    // Tablulation
+    // int minimumTotal(vector<vector<int>>& triangle) {
+    //     int n = triangle.size();
+    //     vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+    //     for(int i = 0; i < n; i ++) {
+    //         dp[n - 1][i] = triangle[n - 1][i];
+    //     }
 
+    //     for(int i = n - 2; i >= 0; i--) {
+    //         for(int j = i; j >= 0; j--) {
+
+    //             if(i == n - 1) {
+    //                 dp[i][j] = triangle[i][j];
+    //             }
+    //             int down = triangle[i][j] + dp[i + 1][j];
+    //             int diag = triangle[i][j] + dp[i + 1][j + 1];
+
+    //             dp[i][j] = min(down, diag);
+    //         }
+    //     }
+
+    //     return dp[0][0];
+    // }
+
+// tabulation with space optimizqtion
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        vector<int> prev(n, 0);
         
         for(int i = 0; i < n; i ++) {
-            dp[n - 1][i] = triangle[n - 1][i];
+            prev[i] = triangle[n - 1][i];
         }
 
         for(int i = n - 2; i >= 0; i--) {
+            vector<int> curr(n, 0);
             for(int j = i; j >= 0; j--) {
 
                 if(i == n - 1) {
-                    dp[i][j] = triangle[i][j];
+                    curr[j] = triangle[i][j];
                 }
-                int down = triangle[i][j] + dp[i + 1][j];
-                int diag = triangle[i][j] + dp[i + 1][j + 1];
+                int down = triangle[i][j] + prev[j];
+                int diag = triangle[i][j] + prev[j + 1];
 
-                dp[i][j] = min(down, diag);
+                curr[j] = min(down, diag);
             }
+
+            prev = curr;
         }
 
-        return dp[0][0];
+        return prev[0];
     }
 
 };
