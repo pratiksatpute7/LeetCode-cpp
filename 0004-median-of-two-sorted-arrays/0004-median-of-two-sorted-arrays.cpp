e742676ca8f2b3ac5969d1f902026a31b5349df7
Pratik Satpute <58502379+pratiksatpute7@users.noreply.github.com>
2025-08-19 22:46:28 -0400

    Time: 0 ms (100%), Space: 95.2 MB (61.39%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int len1 = nums1.size();
-        int len2 = nums2.size();
-        vector<int> merged(len1 + len2);
-        copy(nums1.begin(), nums1.end(), merged.begin());
-        copy(nums2.begin(), nums2.end(), merged.begin() + len1);
-        sort(merged.begin(), merged.end());
-        
-        int totalLen = merged.size();
-        if (totalLen % 2 == 0) {
-            return (merged[totalLen / 2 - 1] + merged[totalLen / 2]) / 2.0;
-        } else {
-            return merged[totalLen / 2];
+        //make nums1 as the smaller array as we want to search in smaller arrya
+        if(nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
         }
+
+        int len1 = nums1.size(), len2 = nums2.size();
+        int low = 0;
+        int high = len1;
+
+
+        while(low <= high) {
+
+            // mid1 and mid2 are number of elemets taken from the respected array
+            int mid1 = (low + high) / 2;
+            int mid2 = (len1 + len2 + 1) / 2 - mid1;
+
+            int l1 = mid1 == 0 ? INT_MIN : nums1[mid1 - 1];
+            int r1 = mid1 == len1 ? INT_MAX : nums1[mid1];
+
+            int l2 = mid2 == 0 ? INT_MIN : nums2[mid2 - 1];
+            int r2 = mid2 == len2 ? INT_MAX : nums2[mid2];
+
+            if (l1 <= r2 && l2 <= r1) {
+                if ((len1 + len2) % 2 == 0) {
+                    return (max(l1, l2) + min(r1, r2)) / 2.0;
+                } else {
+                    return max(l1, l2);
+                }
+            } else if (l1 > r2) {
+                high = mid1 - 1;
+            } else {
+                low = mid1 + 1;
+            }
+        }
+
+        return 0;
     }
 };
